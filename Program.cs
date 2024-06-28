35fde039dc9eeb7ff9166c9950cd381756e97c19
Devpabloedu <158394108+devPabloEdu@users.noreply.github.com>
2024-06-28 20:27:04 -0300

    patterns usados no frontEnd e em NoSQL

--- Program.cs
+++ Program.cs
@@ -8,8 +8,9 @@ namespace DateTimeEmCsharp
             //var data = DateTime.Now; //pega  a data de hoje
             //var data = new DateTime(2020, 10, 12, 8, 23, 14); // ano, mes, dia, hora, minuto e segundo.
             var data = DateTime.Now;
+            var formatada = String.Format("{0:s}", data); //formato muito utilizado de pattern para mandar um json, mto ultilizado tb em sql
             //var formatada = String.Format("{0:t}", data); //short time pattern, vai exibir apenas ahora
-            var formatada = String.Format("{0:D}", data); //vai exibir a data por extenso
+            //var formatada = String.Format("{0:D}", data); //vai exibir a data por extenso
             //var formatada = String.Format("{0:dd/MM/yyyy  hh:mm:ss}", data);
             //var formatada = String.Format("{0}/{1}/{2}", data.Day, data.Month, data.Year); //formatação especifica do dia,mes e ano.
             Console.WriteLine(formatada);
