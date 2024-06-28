4826e4701458b3c7f17573f8fc0a412494653873
Devpabloedu <158394108+devPabloEdu@users.noreply.github.com>
2024-06-28 19:28:55 -0300

    dados especificos no new date

--- Program.cs
+++ Program.cs
@@ -8,6 +8,10 @@ namespace DateTimeEmCsharp
             //var data = DateTime.Now; //pega  a data de hoje
             var data = new DateTime(2020, 10, 12, 8, 23, 14); // ano, mes, dia, hora, minuto e segundo.
             Console.WriteLine(data);
+            Console.WriteLine(data.Year);
+            Console.WriteLine(data.Month);
+            Console.WriteLine(data.Day);
+            Console.WriteLine(data.Hour);
         }
     }
    
