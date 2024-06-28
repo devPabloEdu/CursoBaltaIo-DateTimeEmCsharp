03c27a303d19592b5416863eb24df37257c04152
Devpabloedu <158394108+devPabloEdu@users.noreply.github.com>
2024-06-28 18:38:50 -0300

    DateTime.Now;

--- Program.cs
+++ Program.cs
@@ -5,7 +5,8 @@ namespace DateTimeEmCsharp
     {
         static void Main(string[] args)
         {
-            
+            var data = DateTime.Now; //pega  a data de hoje
+            Console.WriteLine(data);
         }
     }
    
