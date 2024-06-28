fad745cd45196d984ebc70b3c2aad095b83d78a4
Devpabloedu <158394108+devPabloEdu@users.noreply.github.com>
2024-06-28 19:45:44 -0300

    formatando dd/MM/yyyy com o string Format

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace DateTimeEmCsharp
             //var data = DateTime.Now; //pega  a data de hoje
             //var data = new DateTime(2020, 10, 12, 8, 23, 14); // ano, mes, dia, hora, minuto e segundo.
             var data = DateTime.Now;
-            var formatada = String.Format("{0}", data);
+            var formatada = String.Format("{0:dd/MM/yyyy}", data);
             //var formatada = String.Format("{0}/{1}/{2}", data.Day, data.Month, data.Year); //formatação especifica do dia,mes e ano.
             Console.WriteLine(formatada);
         }
--- obj/Debug/net8.0/DateTimeEmCsharp.AssemblyInfo.cs
+++ obj/Debug/net8.0/DateTimeEmCsharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DateTimeEmCsharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4826e4701458b3c7f17573f8fc0a412494653873")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9664b00a20bde34822abd3b7c0384a5297f3c38a")]
 [assembly: System.Reflection.AssemblyProductAttribute("DateTimeEmCsharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DateTimeEmCsharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
